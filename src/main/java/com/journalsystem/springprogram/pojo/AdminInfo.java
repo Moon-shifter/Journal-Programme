92dbc84d9999cfac8b5e3d1cab20b9e3d8bf0819
HuaiJin Wu <touchtheskydown@outlook.com>
2025-12-14 12:48:40 +0800

    日常修改

---
+++ src/main/java/com/journalsystem/springprogram/pojo/AdminInfo.java
@@ -0,0 +1,93 @@
+package com.journalsystem.springprogram.pojo;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "admin_info")
+public class AdminInfo {
+    @Id
+    @Column(name = "admin_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "real_name", nullable = false, length = 50)
+    private String realName;
+
+    @ColumnDefault("'normal'")
+    @Lob
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "last_login")
+    private Instant lastLogin;
+
+    @ColumnDefault("'inactive'")
+    @Lob
+    @Column(name = "STATUS")
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Instant getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Instant lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
