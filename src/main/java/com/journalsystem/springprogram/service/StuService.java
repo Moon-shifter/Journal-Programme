41ed1b1f40f24e0a081db8fecc8eee8e607c70bc
HuaiJin Wu <touchtheskydown@outlook.com>
2025-12-14 13:16:03 +0800

    删除测试文件

--- src/main/java/com/journalsystem/springprogram/service/StuService.java
+++
@@ -1,7 +0,0 @@
-package com.journalsystem.springprogram.service;
-
-import com.journalsystem.springprogram.pojo.Student;
-
-public interface StuService {
-    void insertStudent(Student student);
-}
