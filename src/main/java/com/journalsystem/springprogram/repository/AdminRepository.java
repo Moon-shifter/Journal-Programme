70b07f24d07e8f4eee1eaa302bf9e8a669ea3a4b
HuaiJin Wu <touchtheskydown@outlook.com>
2025-12-16 12:33:09 +0800

    添加注解

--- src/main/java/com/journalsystem/springprogram/repository/AdminRepository.java
+++ src/main/java/com/journalsystem/springprogram/repository/AdminRepository.java
@@ -2,7 +2,9 @@ package com.journalsystem.springprogram.repository;
 
 import com.journalsystem.springprogram.pojo.AdminInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 // 泛型：JpaRepository<实体类, 主键类型>
 public interface AdminRepository extends JpaRepository<AdminInfo, Integer> {
     // 按JPA方法命名规则，自动生成“根据username查询”的SQL(select * from admin_info where username = ?)
